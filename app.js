45af464ebd96f5881a80f100b51976c2edd2dfc5
Victor <adevic4christ@gmail.com>
2020-02-29 21:40:51 +0100

    Added global error handler

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const articleRouter = require("./routes/articleRoutes");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
@@ -8,5 +9,7 @@ const app = express();
 app.use(express.json()); // Body parser
 
 app.use("/api/v1/articles", articleRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
