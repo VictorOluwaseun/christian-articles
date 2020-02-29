4e8f8708e19492d78362498ee4746514e4f86a36
Victor <adevic4christ@gmail.com>
2020-02-29 15:37:11 +0100

    Exported app server to server.js and created server for mongoDB connection

--- app.js
+++ app.js
@@ -9,6 +9,4 @@ app.use(express.json()); // Body parser
 
 app.use("/api/v1/articles", articleRouter);
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server started"));
+module.exports = app;
--- server.js
+++ server.js
@@ -6,3 +6,13 @@ dotenv.config({
 });
 
 const app = require("./app");
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }).then(() => console.log("DB connection successful"));
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log("Server started"));
