077c9c1ebdac3af59ba599413ff815166537c47e
Victor <adevic4christ@gmail.com>
2020-02-29 22:10:50 +0100

    Refactored middlewares and controllers with catchAsync

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,99 +1,55 @@
 const Article = require("./../models/articleModel");
+const catchAsync = require("./../utils/catchAsync");
 
-exports.getAllArticles = async (req, res, next) => {
-  try {
-    const articles = await Article.find();
-    res.status(200).json({
-      status: "success",
-      result: articles.length,
-      data: {
-        articles
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        err
-      }
-    });
-  }
+exports.getAllArticles = catchAsync(async (req, res, next) => {
+  const articles = await Article.find();
+  res.status(200).json({
+    status: "success",
+    result: articles.length,
+    data: {
+      articles
+    }
+  });
+});
 
-}
+exports.getArticle = catchAsync(async (req, res, next) => {
+  const article = await Article.findById(req.params.id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      article
+    }
+  });
+});
 
-exports.getArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findById(req.params.id);
+exports.createArticle = catchAsync(async (req, res, next) => {
+  const newArticle = await Article.create(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      newArticle
+    }
+  });
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        article
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        err
-      }
-    });
-  }
-}
+});
 
-exports.createArticle = async (req, res, next) => {
-  try {
-    const newArticle = await Article.create(req.body);
-    res.status(200).json({
-      status: "success",
-      data: {
-        newArticle
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        err
-      }
-    });
-  }
-}
+exports.updateArticle = catchAsync(async (req, res, next) => {
+  const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: "success",
+    data: {
+      article
+    }
+  });
+});
 
-exports.updateArticle = async (req, res, next) => {
-  try {
-    const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        article
-      }
-    });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        err
-      }
-    });
-  }
-
-}
-exports.deleteArticle = async (req, res, next) => {
-  try {
-    await Article.findByIdAndDelete(req.params.id);
-
-    res.status(204).json({
-      status: "success",
-      data: null
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      data: err
-    });
-  }
-}
+exports.deleteArticle = catchAsync(async (req, res, next) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: "success",
+    data: null
+  });
+});
