f09cfcd6868712e86c4caaddec9990b2ecfa3604
Victor <adevic4christ@gmail.com>
2020-03-01 14:22:03 +0100

    Added development and production formatted error functions

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -7,8 +7,27 @@ const sendErrorDev = (err, res) => {
   });
 };
 
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error: Send error to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    //Programming error or other unknown error: not to leak error details
+    //1. Lor error
+    console.error("ERROR 💥", err);
+    //2. Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!"
+    });
+  }
+}
+
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
+  // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
