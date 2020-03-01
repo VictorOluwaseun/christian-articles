14cb563e6a071f0275fb683634d5fed621d971f1
Victor <adevic4christ@gmail.com>
2020-03-01 14:32:50 +0100

    Implemented cast error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,37 +1,47 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = err => {
+ const message = `Invalid ${err.path}: ${err.value}.`;
+ return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
-  });
+ res.status(err.statusCode).json({
+  status: err.status,
+  error: err,
+  message: err.message,
+  stack: err.stack
+ });
 };
 
 const sendErrorProd = (err, res) => {
-  //Operational, trusted error: Send error to the client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-  } else {
-    //Programming error or other unknown error: not to leak error details
-    //1. Lor error
-    console.error("ERROR 💥", err);
-    //2. Send generic message
-    res.status(500).json({
-      status: "error",
-      message: "Something went very wrong!"
-    });
-  }
-}
+ //Operational, trusted error: Send error to the client
+ if (err.isOperational) {
+  res.status(err.statusCode).json({
+   status: err.status,
+   message: err.message
+  });
+ } else {
+  //Programming error or other unknown error: not to leak error details
+  //1. Lor error
+  console.error("ERROR 💥", err);
+  //2. Send generic message
+  res.status(500).json({
+   status: "error",
+   message: "Something went very wrong!"
+  });
+ }
+};
 
 module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+ // console.log(err.stack);
+ err.statusCode = err.statusCode || 500;
+ err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res);
-  }
-}
+ if (process.env.NODE_ENV === "development") {
+  sendErrorDev(err, res);
+ } else if (process.env.NODE_ENV === "production") {
+  let error = { ...err };
+  if (error.name === "CastError") error = handleCastErrorDB(error);
+ }
+};
